1e8d3d330261acbeaa5c56ac882c71f76d4f05ae
Rajat Pande <wwe.rajattiger.com@gmail.com>
2026-07-30 00:26:44 +0530

    seo: add dynamic sitemap and robots.txt

---
+++ src/app/robots.js
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: 'https://stepupcalculator.com/sitemap.xml',
+  };
+}
---
+++ src/app/sitemap.js
@@ -0,0 +1,10 @@
+export default function sitemap() {
+  return [
+    {
+      url: 'https://stepupcalculator.com',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 1.0,
+    },
+  ];
+}
